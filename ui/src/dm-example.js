2481ac7142ae0921d3a2b849bc48d82b3800313b
Dan Gottlieb <dan@gotdan.org>
2020-11-05 12:42:49 -0500

    first pass at sql gen

--- ui/src/dm-example.js
+++ ui/src/dm-example.js
@@ -30,7 +30,7 @@ export default {
 		"rules": [{
 			"id": 0,
 			"fieldName": "code",
-			"ruleSet": [{
+			"restrictions": [{
 				"name": "SNOMED DM",
 				"system": "http://snomed.info/sct",
 				"code": "105401000119101,106281000119103,10754881000119104,111307005,111552007,123763000,127012008,1481000119100,190330002,190331003,190368000,190372001,190389009,190406000,190407009,190410002,190411003,190412005,199223000,199225007,199226008,199227004,199228009,199229001,199230006,199231005,23045005,237599002,237600004,237601000,237604008,237613005,237618001,237619009,237627000,237651005,24203005,2751001,28032008,31321000119102,313435000,313436004,314771006,314893005,314902007,314903002,314904008,33559001,359642000,368521000119107,408539000,426705001,426875007,427089005,42954008,44054006,445260006,46635009,4783006,51002006,530558861000132104,57886004,59079001,5969009,609561005,609562003,609563008,609564002,609566000,609567009,609568004,609569007,609570008,609571007,609572000,609573005,609574004,609575003,609576002,609577006,609578001,70694009,709147009,710815001,716362006,71791000119104,719216001,720519003,722454003,724136006,73211009,733072002,734022008,75524006,75682002,76751001,81531005,8801005,91352004,9859006"
@@ -50,7 +50,7 @@ export default {
 		"rules": [{
 			"id": 0,
 			"fieldName": "status",
-			"ruleSet": [{
+			"restrictions": [{
 				"invalid": false,
 				"option": "Final",
 				"validate": false
@@ -66,7 +66,7 @@ export default {
 		},{
 			"id": 1,
 			"fieldName": "code",
-			"ruleSet": [{
+			"restrictions": [{
 				"system": "http://loinc.org",
 				"code": "4548-4"
 			}]
@@ -82,7 +82,7 @@ export default {
 			"id": 0,
 			"fieldName": "id",
 			"edit": false,
-			"ruleSet": [{
+			"restrictions": [{
 				"invalid": false,
 				"target": "2.id",
 				"validate": false
@@ -91,7 +91,7 @@ export default {
 			"id": 1,
 			"fieldName": "valueQuantity",
 			"edit": false,
-			"ruleSet": [{
+			"restrictions": [{
 				"comparator": "gte",
 				"compareTo": "fixed",
 				"fixedValue": "7",
@@ -114,7 +114,7 @@ export default {
 		"rules": [{
 			"id": 0,
 			"fieldName": "status",
-			"ruleSet": [{
+			"restrictions": [{
 				"invalid": false,
 				"option": "Final",
 				"validate": false
@@ -131,7 +131,7 @@ export default {
 			"id": 1,
 			"fieldName": "effectiveDateTimeStart",
 			"edit": false,
-			"ruleSet": [{
+			"restrictions": [{
 				"invalid": false,
 				"comparator": "lt",
 				"compareTo": "2.effectiveDateTimeStart",
@@ -144,7 +144,7 @@ export default {
 			"id": 2,
 			"fieldName": "effectiveDateTimeStart",
 			"edit": false,
-			"ruleSet": [{
+			"restrictions": [{
 				"invalid": false,
 				"comparator": "gte",
 				"compareTo": "0.birthDate",
@@ -157,7 +157,7 @@ export default {
 		},{
 			"id": 3,
 			"fieldName": "code",
-			"ruleSet": [{
+			"restrictions": [{
 				"system": "http://loinc.org",
 				"code": "4548-4"
 			}]
@@ -173,7 +173,7 @@ export default {
 			"id": 0,
 			"fieldName": "id",
 			"edit": false,
-			"ruleSet": [{
+			"restrictions": [{
 				"invalid": false,
 				"target": "4.id",
 				"validate": false
@@ -182,7 +182,7 @@ export default {
 			"id": 1,
 			"fieldName": "valueQuantity",
 			"edit": false,
-			"ruleSet": [{
+			"restrictions": [{
 				"comparator": "gte",
 				"compareTo": "fixed",
 				"fixedValue": "7",
@@ -202,7 +202,7 @@ export default {
 		"rules":[{
 			"id":0,
 			"fieldName":"clinicalStatus",
-			"ruleSet": [{
+			"restrictions": [{
 				"invalid":false,
 				"option":"Active",
 				"validate":false
@@ -210,13 +210,13 @@ export default {
 		},{
 			"id":1,
 			"fieldName":"verificationStatus",
-			"ruleSet":[{
+			"restrictions":[{
 				"option":"Confirmed"
 			}]
 		},{
 			"id":2,
 			"fieldName":"code",
-			"ruleSet":[{
+			"restrictions":[{
 				"system":"http://snomed.info/sct",
 				"name":"SNOMED",
 				"code":"10725009, 10964002, 1201005, 123799005, 59621000"
